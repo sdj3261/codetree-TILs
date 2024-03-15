cc437e97d92f7fb2aebe4ab6771e056eab5b5506
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-03-15 14:03:39 +0900

    Title: 행복한 수열의 개수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240315/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,7 +2,7 @@ def count_seq(arr,m) :
     ret = 0
     max_ret = 0
     if len(arr) < 2 :
-        return ret
+        return
     for i in range(len(arr)-1) :
         if ret == 0 and arr[i] == arr[i+1] :
             ret += 2
@@ -20,11 +20,14 @@ def count_seq(arr,m) :
 
 n,m = map(int,input().split())
 arr = []
-ret = 0
+if n == 1 :
+    ret = 1
+else :
+    ret = 0
 
 for i in range(n) :
     data = list(map(int,input().split()))
-    arr.append(data)
+    arr.append(data) 
 
 transposed_matrix = list(map(list, zip(*arr)))
 
@@ -35,4 +38,5 @@ for i in range(n) :
         ret += 1
     if count_seq(data2,m) :
         ret += 1
+        
 print(ret)
