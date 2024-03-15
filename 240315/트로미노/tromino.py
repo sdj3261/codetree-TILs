52f20d97488ca0fff0b62b876d2602ab24527ddf
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-03-15 15:39:22 +0900

    Title: 트로미노, Time: 224ms, Memory: 26MB, Status: Passed - CodeTree

--- 240315/트로미노/tromino.py
+++ 240315/트로미노/tromino.py
@@ -7,12 +7,15 @@ def calculateSquareB(arr,i,j) :
     data = max(sums - arr[i][j], sums - arr[i+1][j], sums - arr[i][j+1], sums - arr[i+1][j+1])
     return data
 
-def calculate3B(arr,i,j) :
-    r = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-    c = arr[i][j] + arr[i+1][j] + arr[i+2][j]
-    data = max(r,c)
+def calculate3BC(arr,i,j) :
+    c = arr[i][j] + arr[i][j+1] + arr[i][j+2]
 
-    return data
+    return c
+
+def calculate3BR(arr,i,j) :
+    r = arr[i][j] + arr[i+1][j] + arr[i+2][j]
+
+    return r
 
 n,m = map(int,input().split())
 arr = []
@@ -25,8 +28,12 @@ for i in range(n-1) :
     for j in range(m-1) :
         ret = max(ret,calculateSquareB(arr,i,j))
 
-for i in range(n-2) :
+for i in range(n) :
     for j in range(m-2) :
-        ret = max(ret,calculate3B(arr,i,j))
+        ret = max(ret,calculate3BC(arr,i,j))
+        
+for i in range(n-2) :
+    for j in range(m) :
+        ret = max(ret,calculate3BR(arr,i,j))
 
 print(ret)
