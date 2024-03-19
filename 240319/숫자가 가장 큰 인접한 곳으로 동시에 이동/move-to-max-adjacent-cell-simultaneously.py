27b37386e3499eb68616ec7ba522786d574629b1
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-03-19 13:01:39 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240319/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -0,0 +1,46 @@
+def in_range(arr,y,x) :
+    if 0<=y<=len(arr)-1 and 0<=x<=len(arr[0])-1 :
+        return True
+    return False
+
+def dfs(arr,marble,t) :
+    dys = [-1,1,0,0]
+    dxs = [0,0,-1,1]
+
+    next_marble_pos = []
+
+    if t == 0 :
+        print(len(marble))
+        return
+    
+    for my,mx in marble :
+        cnt = -1
+        for dy,dx in zip(dys,dxs) :
+            cnt+=1
+            ny = my + dy
+            nx = mx + dx
+            
+            if in_range(arr,ny,nx) == False:
+                continue
+            if cnt == 0 :
+                marble_pos = arr[ny][nx]
+                p = (ny,nx)
+            if marble_pos < arr[ny][nx] :
+                p = (ny,nx)
+        if cnt == 3 :
+            next_marble_pos.append(p)
+    next_marble_pos = set(next_marble_pos)
+    dfs(arr,next_marble_pos,t-1)
+
+        
+n,m,t = map(int,input().split())
+arr =  []
+marble=[]
+
+for i in range(n) :
+    arr.append(list(map(int,input().split())))
+for i in range(m) :
+    y,x = map(int,input().split())
+    marble.append((y-1,x-1))
+
+dfs(arr,marble,t)
