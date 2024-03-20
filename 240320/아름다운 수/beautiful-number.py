b8acd510ad04e94fb367cf47e37f0f76e122898a
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-03-20 11:05:09 +0900

    Title: 아름다운 수, Time: 690ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240320/아름다운 수/beautiful-number.py
@@ -0,0 +1,37 @@
+import sys
+sys.setrecursionlimit(10**5)
+global cnt 
+def is_beautiful(arr):
+    s = ""
+    for i in arr :
+        s+=str(i)
+    i = 0
+    while i < len(s) :
+        length = int(s[i])
+        if s[i:i+length] == s[i] * length :
+            i = i+length
+            pass
+        else :
+            return False
+    return True
+
+def generateNumberNdigit(arr,n) :
+    if n==0 :
+        if is_beautiful(arr) :
+            global cnt
+            cnt += 1
+            return
+        else :
+            return
+    for i in range(1, 5):
+        arr.append(i)
+        generateNumberNdigit(arr,n-1)
+        arr.pop()
+
+
+#n은 자릿수
+n = int(input())
+cnt = 0
+arr = []
+generateNumberNdigit(arr,n)
+print(cnt)
