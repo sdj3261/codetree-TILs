8cbdc0bb2a856d9e48c9c2f6a8bc93d9f2aec7a5
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-03-20 16:47:36 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240320/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -0,0 +1,25 @@
+def checkSeqThree(arr) :
+    i = 0
+    cnt = 1
+    while i < len(arr)-1 :
+        if arr[i] == arr[i+1] :
+            cnt +=1
+            if cnt == 3 :
+                return True
+        else :
+            cnt = 1
+        i+=1   
+    return False
+
+def conditionCombi(arr,n,k) :
+    if n == 0 and not checkSeqThree(arr) :
+        print(*arr)
+        return
+    for i in range(1,k+1) :
+        arr.append(i)
+        conditionCombi(arr,n-1,k)
+        arr.pop()
+
+k,n = map(int,input().split())
+arr = []
+conditionCombi(arr,n,k)
