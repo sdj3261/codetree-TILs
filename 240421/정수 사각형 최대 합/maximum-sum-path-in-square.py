dde4a5062371a8e5804352312c4e36adf2cf5862
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-04-21 19:29:21 +0900

    Title: 정수 사각형 최대 합, Time: 269ms, Memory: 25MB, Status: Passed - CodeTree

--- 240421/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240421/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-num = [
+arr = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
@@ -10,26 +10,19 @@ dp = [
     for _ in range(n)
 ]
 
-def initialize():
-    # 시작점의 경우 dp[0][0] = num[0][0]으로 초기값을 설정해줍니다
-    dp[0][0] = num[0][0]
-    
-    # 최좌측 열의 초기값을 설정해줍니다.
-    for i in range(1, n):
-        dp[i][0] = dp[i-1][0] + num[i][0]
-    
-    # 최상단 행의 초기값을 설정해줍니다.
-    for j in range(1, n):
-        dp[0][j] = dp[0][j-1] + num[0][j]
-        
-        
-# 초기값 설정
-initialize()
+# 초기값 세팅 
+dp[0][0] = arr[0][0]
 
-# 탐색하는 위치의 위에 값과 좌측 값 중에 큰 값에
-# 해당 위치의 숫자를 더해줍니다. 
-for i in range(1, n):
-    for j in range(1, n):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + num[i][j]
-        
+for i in range(1,n) :
+    dp[i][0] = dp[i-1][0] + arr[i][0]
+for j in range(1,n) :
+    dp[0][j] = arr[0][j] + dp[0][j-1]
+
+#점화식 세우기 dp[i][j] = max(위,왼쪽)
+#출력 dp[n-1,n-1]
+
+for i in range(1,n) :
+    for j in range(1,n) :
+        dp[i][j] = max(dp[i-1][j] + arr[i][j],dp[i][j-1] + arr[i][j]) 
+
 print(dp[n-1][n-1])
