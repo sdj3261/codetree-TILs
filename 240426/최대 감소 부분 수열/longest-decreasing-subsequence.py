8ed93f90b96d520fdc7abca659b4b9c347deb5c7
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-04-26 19:20:54 +0900

    Title: 최대 감소 부분 수열, Time: 127ms, Memory: 25MB, Status: Passed - CodeTree

--- 240426/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 240426/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -1,12 +1,12 @@
 n = int(input())
-nums = list(map(int,input().split()))
-dp = [0] * (n+1)
+nums = [0] + list(map(int,input().split()))
+dp = [1] * (n+1)
 
 dp[0] = 1
 
 # dp[i] : 마지막으로 고른 원소의 위치가 i인
 # 부분 수열 중 최소 부분 수열의 길이
-for i in range(1,n) : 
+for i in range(1,n+1) : 
     for j in range(0,i) :
         if nums[i] < nums[j] :
             dp[i] = max(dp[i],dp[j]+1) 
