c3a973290b751add48ebbd229cb12ca7c1867962
신동재 <63071967+sdj3261@users.noreply.github.com>
2024-05-09 20:55:59 +0900

    Title: 최단 Run Length 인코딩, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240509/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240509/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -1,36 +1,31 @@
-def make(s):
-    if not s:
-        return ""
-    
-    encoded = ""
-    count = 1
-    char = s[0]
-    
-    for i in range(1, len(s)):
-        if s[i] == char:
-            count += 1
-        else:
-            encoded += char + str(count)
-            char = s[i]
-            count = 1
-    encoded += char + str(count)  # Add the last group of characters and their count
-    return encoded
+def shift(a) :
+    return a[-1] + a[0:len(a)-1]
+def run(a) :
+    cnt = 1
+    ret = ""
+    encodeChar = a[0]
+    for i in range(1,len(a)) :
+        if encodeChar == a[i] :
+            cnt += 1
+        else :
+            encodeChar = encodeChar + str(cnt)
+            ret = ret + encodeChar
+            cnt = 1
+            encodeChar = a[i]
+    if cnt > 0 :
+        encodeChar = encodeChar + str(cnt)
+        ret = ret + encodeChar
+    return len(ret)
+            
 
-def runLengthEncoding(s):
-    min_length = float('inf')
-    
-    if len(s) == 1:
-        return 2  # Special case for single character (character + '1')
-
-    # Check RLE length for all rotations
-    for i in range(len(s)):
-        rotated = s[-i:] + s[:-i]  # Rotate the string
-        rle = make(rotated)
-        min_length = min(min_length, len(rle))
-
-    return min_length
-
-# Example usage
-n = input()
-result = runLengthEncoding(n)
-print(result)
+a = input()
+n = len(a) - 1
+minEncode = 1000
+if len(a) == 1 :
+    print(2)
+else :
+    for i in range(n) :
+        a = shift(a)
+        encode = run(a)
+        minEncode = min(minEncode,encode)
+    print(minEncode)
